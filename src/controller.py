87301f927b885b2bcf9f3ec9f554ababdd611658
Chanda Roy <chandanroy590@gmail.com>
2020-04-10 21:43:10 +0530

    individually multtiple subprocess (vertex. py) call using subprocess.Popen()

--- src/controller.py
+++ src/controller.py
@@ -17,6 +17,6 @@ for row in reader:
 
 for vertex in dictionary:
     neigh = ' '.join(dictionary[vertex])
-    subprocess.call(f'python ./vertex.py {vertex} {neigh}', shell=True)
+    subprocess.Popen(f'python ./vertex.py ./ {vertex} {neigh}', shell=True)
     # subprocess.call('python pi.txt', shell=True)
     # Run: D:\Educational\dsg_single_faliure_recovery\src> python .\controller.py ..\data\facebook_data.csv
