46ee5b126e8c61749e280bbab761369b8b89f649
Chanda Roy <chandanroy590@gmail.com>
2020-04-12 14:20:20 +0530

    comment added on controller for running all sub process in same terminal

--- src/controller.py
+++ src/controller.py
@@ -18,5 +18,6 @@ for row in reader:
 for vertex in dictionary:
     neigh = ' '.join(dictionary[vertex])
     subprocess.Popen(f'python ./vertex.py ./ {vertex} {neigh}', creationflags=subprocess.CREATE_NEW_CONSOLE)
+    # subprocess.Popen(f'python ./vertex.py ./ {vertex} {neigh}', shell=True)
     # subprocess.call('python pi.txt', shell=True)
     # Run: D:\Educational\dsg_single_faliure_recovery\src> python .\controller.py ..\data\facebook_data.csv
